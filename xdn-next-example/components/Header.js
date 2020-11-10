d73c148a9d499a841385125d10ce78db20e274ab
Tristan Lee <tristanlee85@gmail.com>
2020-11-10 10:07:42 -0500

    Highlight selected category

--- xdn-next-example/components/Header.js
+++ xdn-next-example/components/Header.js
@@ -2,10 +2,13 @@ import Link from 'next/link';
 import { Prefetch } from '@xdn/react';
 import styles from '../styles/Header.module.css';
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import { getCategories } from '../lib/cms';
 
 export default function Header() {
   const [categories, setCategories] = useState();
+  const [activeTab, setActiveTab] = useState();
+  const router = useRouter();
 
   useEffect(() => {
     async function fetchCategories() {
@@ -16,13 +19,24 @@ export default function Header() {
     fetchCategories();
   }, []);
 
+  useEffect(() => {
+    router.events.on('routeChangeComplete', (url) => {
+      if (categories) {
+        setActiveTab(categories.findIndex(({ href }) => href === url));
+      }
+    });
+  }, [categories]);
+
   if (!categories) return null;
 
   return (
     <div className={styles.container}>
       <ul>
-        {categories.map(({ category, categoryName, href }) => (
-          <li key={categoryName}>
+        {categories.map(({ category, categoryName, href }, i) => (
+          <li
+            key={categoryName}
+            className={activeTab === i ? styles.active : null}
+          >
             <Link as={href} href="/category/[id]" passHref>
               <Prefetch>
                 <a>{categoryName}</a>
