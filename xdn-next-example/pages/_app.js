52f56a787d09f5ca86234e1fe0d59d0febfef2b0
Tristan Lee <tristanlee85@gmail.com>
2020-11-10 15:05:59 -0500

    Remove includeCacheMisses

--- xdn-next-example/pages/_app.js
+++ xdn-next-example/pages/_app.js
@@ -1,15 +1,8 @@
 import '../styles/globals.css';
 import '../styles/tailwind.css';
 import Header from '../components/Header';
-import { install } from '@xdn/prefetch/window';
 
 function MyApp({ Component, pageProps }) {
-  if (process.browser) {
-    document.addEventListener('DOMContentLoaded', function () {
-      install({ includeCacheMisses: true });
-    });
-  }
-
   return (
     <>
       <header className="md:flex bg-white rounded-lg p-2 justify-center">
