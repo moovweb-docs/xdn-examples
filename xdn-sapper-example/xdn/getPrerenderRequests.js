8d6ae88150f8cc776780eb9e9ffa9b0ba97acc67
Tristan Lee <tristanlee85@gmail.com>
2020-11-20 16:58:23 -0500

    Test prerender debugging

--- xdn-sapper-example/xdn/getPrerenderRequests.js
+++ xdn-sapper-example/xdn/getPrerenderRequests.js
@@ -1,12 +1,15 @@
-import { getCategories } from '../lib/cms';
+// import { getCategories } from '../lib/cms';
 
 export default async function getPrerenderRequests() {
-  const { categories } = await getCategories();
-  const requests = categories.map((c) => ({ path: c.href }));
+  // const { categories } = await getCategories();
+  // const requests = categories.map((c) => ({ path: c.href }));
 
-  categories.forEach((c) => {
-    requests.push(...c.items.map((p) => ({ path: p.href })));
-  });
+  // categories.forEach((c) => {
+  //   requests.push(...c.items.map((p) => ({ path: p.href })));
+  // });
+  const requests = [{ path: '/foo' }]
+
+  //throw new Error(JSON.stringify(requests))
 
   return requests;
 }
