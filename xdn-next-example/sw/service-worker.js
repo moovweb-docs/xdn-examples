969146fffd3676de1c32e529612d6703a014d56a
Tristan Lee <tristanlee85@gmail.com>
2020-11-25 09:43:14 -0500

    Deep prefetch product images

--- xdn-next-example/sw/service-worker.js
+++ xdn-next-example/sw/service-worker.js
@@ -1,9 +1,21 @@
 import { skipWaiting, clientsClaim } from 'workbox-core';
 import { precacheAndRoute } from 'workbox-precaching';
 import { Prefetcher } from '@xdn/prefetch/sw';
+import DeepFetchPlugin from '@xdn/prefetch/sw/DeepFetchPlugin'
 
 skipWaiting();
 clientsClaim();
 precacheAndRoute(self.__WB_MANIFEST || []);
 
-new Prefetcher().route();
+new Prefetcher({
+  plugins: [
+    new DeepFetchPlugin([
+      // deep fetches the "category" API call to get PLP images:
+      {
+        jsonQuery: 'pageProps.products.picture',
+        maxMatches: 10,
+        as: 'image',
+      },
+    ])
+  ]
+}).route();
