2869893c682a7d9786f12bb8cecba243c12c52ad
Tristan Lee <tristanlee85@gmail.com>
2020-12-02 11:02:18 -0500

    Remove upstream cache-control header

--- xdn-sapper-example/routes.js
+++ xdn-sapper-example/routes.js
@@ -4,34 +4,39 @@ import { Router } from '@xdn/core/router'
 import { sapperRoutes } from '@xdn/sapper'
 import getPrerenderRequests from './xdn/getPrerenderRequests'
 
-const SSR_CACHE_CONFIG = {
-  browser: {
-    maxAgeSeconds: 0,
-  },
-  edge: {
-    maxAgeSeconds: 60 * 60 * 24 * 365 * 10,
-    staleWhileRevalidateSeconds: 60 * 60 * 24,
-  },
-};
+const SSR_CACHE_HANDLER = ({ removeUpstreamResponseHeader, cache }) => {
+  removeUpstreamResponseHeader('cache-control')
+  cache({
+    browser: {
+      maxAgeSeconds: 0,
+    },
+    edge: {
+      maxAgeSeconds: 60 * 60 * 24 * 365 * 10,
+      staleWhileRevalidateSeconds: 60 * 60 * 24,
+    },
+  })
+}
 
-const API_CACHE_CONFIG = {
-  browser: {
-    maxAgeSeconds: 0,
-    serviceWorkerSeconds: 60 * 60 * 24,
-  },
-  edge: {
-    maxAgeSeconds: 60 * 60 * 24 * 365 * 10,
-    staleWhileRevalidateSeconds: 60 * 60 * 24,
-  },
-};
+const API_CACHE_HANDLER = ({ removeUpstreamResponseHeader, cache, proxy }) => {
+  removeUpstreamResponseHeader('cache-control')
+  cache({
+    browser: {
+      maxAgeSeconds: 0,
+      serviceWorkerSeconds: 60 * 60 * 24,
+    },
+    edge: {
+      maxAgeSeconds: 60 * 60 * 24 * 365 * 10,
+      staleWhileRevalidateSeconds: 60 * 60 * 24,
+    },
+  })
+  proxy('api', { path: '/:path*' })
+}
 
 export default new Router()
   .prerender(getPrerenderRequests)
-  .match('/', ({ cache }) => cache(SSR_CACHE_CONFIG))
-  .match('/category/:name', ({ cache }) => cache(SSR_CACHE_CONFIG))
-  .match('/product/:name', ({ cache }) => cache(SSR_CACHE_CONFIG))
-  .match('/api/:path*', ({ proxy, cache }) => {
-    cache(API_CACHE_CONFIG)
-    proxy('api', { path: '/:path*' })
-  })
+  .match('/api/:path*', API_CACHE_HANDLER)
   .use(sapperRoutes) // automatically adds routes for all files under /src/routes
+
+  .match('/', SSR_CACHE_HANDLER)
+  .match('/category/:name', SSR_CACHE_HANDLER)
+  .match('/product/:name', SSR_CACHE_HANDLER)
