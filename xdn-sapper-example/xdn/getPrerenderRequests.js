423c876e741364cf5ccee4e52b00b2b02b658ef7
Tristan Lee <tristanlee85@gmail.com>
2020-12-07 14:48:56 -0500

    Update prerender paths to include build timestamp

--- xdn-sapper-example/xdn/getPrerenderRequests.js
+++ xdn-sapper-example/xdn/getPrerenderRequests.js
@@ -1,11 +1,11 @@
-import { getCategories } from '../lib/cms'
+import { getCategories, getApiPath } from '../lib/cms'
 
 export default async function getPrerenderRequests() {
   const { categories } = await getCategories()
-  const requests = categories.map(c => ({ path: `/api${c.href}` }))
+  const requests = categories.map(c => ({ path: getApiPath(c.href) }))
 
   categories.forEach(c => {
-    requests.push(...c.items.map(p => ({ path: `/api${p.href}` })))
+    requests.push(...c.items.map(p => ({ path: getApiPath(p.href) })))
   })
 
   return requests
