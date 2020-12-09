0450b687e35ee717fcaca573e6aa4a9136cc8107
Tristan Lee <tristanlee85@gmail.com>
2020-12-09 17:05:41 -0500

    Fix invalid destructuring

--- xdn-nuxt-example/xdn/getPrerenderRequests.js
+++ xdn-nuxt-example/xdn/getPrerenderRequests.js
@@ -1,7 +1,7 @@
 import { getCategories, getApiPath } from '../lib/cms'
 
 export default async function getPrerenderRequests() {
-  const { categories } = await getCategories()
+  const categories = await getCategories()
   const requests = categories.map((c) => ({ path: getApiPath(c.href) }))
 
   categories.forEach((c) => {
