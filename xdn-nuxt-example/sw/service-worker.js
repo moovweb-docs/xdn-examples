d78049d82b26ea5d7876107dc36b40ff480aed4d
Tristan Lee <tristanlee85@gmail.com>
2020-12-10 10:59:00 -0500

    Prefetch images

--- xdn-nuxt-example/sw/service-worker.js
+++ xdn-nuxt-example/sw/service-worker.js
@@ -1,9 +1,34 @@
 import { skipWaiting, clientsClaim } from 'workbox-core'
 import { precacheAndRoute } from 'workbox-precaching'
 import { Prefetcher } from '@xdn/prefetch/sw'
+import DeepFetchPlugin from '@xdn/prefetch/sw/DeepFetchPlugin'
+import { getOptimizedImageUrl } from '../lib/cms'
 
 skipWaiting()
 clientsClaim()
 precacheAndRoute(self.__WB_MANIFEST || [])
 
-new Prefetcher().route()
+new Prefetcher({
+  plugins: [
+    new DeepFetchPlugin([
+      // query the PLP API response for images to prefetch
+      // prefetch logic is handled in client.js
+      {
+        jsonQuery: 'picture:picture',
+        jsonQueryOptions: {
+          locals: {
+            picture: (input) => {
+              if (input.map) {
+                return input.map(getOptimizedImageUrl)
+              }
+
+              return []
+            },
+          },
+        },
+        maxMatches: 10,
+        as: 'image',
+      },
+    ]),
+  ],
+}).route()
