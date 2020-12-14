71bba5341be76a76fc7b4a6505a0a4e67542230b
Tristan Lee <tristanlee85@gmail.com>
2020-12-14 14:10:31 -0500

    Add SW bundler plugin

--- xdn-vue-example/routes.js
+++ xdn-vue-example/routes.js
@@ -8,12 +8,12 @@ export default new Router()
   .match('/category/:name', SSR_CACHE_HANDLER)
   .match('/product/:name', SSR_CACHE_HANDLER)
 
-  // Send requests to static assets in the build output folder `dist`
-  .static('dist')
-
   .match('/service-worker.js', ({ serviceWorker }) => {
     serviceWorker('dist/service-worker.js')
   })
+
+  // Send requests to static assets in the build output folder `dist`
+  .static('dist')
 
   // Send everything else to the App Shell
   .fallback(({ appShell }) => {
