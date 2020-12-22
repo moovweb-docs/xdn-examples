faa48c7bbc7e2613124d2199ba272655c3a63931
Tristan Lee <tristanlee85@gmail.com>
2020-12-22 17:37:43 -0500

    Write build ID to path that doesn't get changed
    `__sapper__/` and `.nuxt/` get removed/replaced at different times, and the same can occur with `.xdn/` depending on if the project is run before built.

--- xdn-sapper-example/webpack.config.js
+++ xdn-sapper-example/webpack.config.js
@@ -22,7 +22,7 @@ const preprocess = sveltePreprocess({
 })
 
 const BUILD_ID = new Date().getTime()
-fs.writeFileSync(join('__sapper__', 'build', 'BUILD_ID'), BUILD_ID)
+fs.writeFileSync(join('xdn', 'BUILD_ID'), BUILD_ID)
 
 module.exports = {
   client: {
